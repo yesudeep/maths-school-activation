b216d41a154ad693d8c8d979d9e749428b15d5b7
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2010-03-30 02:37:45 +0530

    a sprinkle of validation

--- media/script/base.js
+++ media/script/base.js
@@ -1,1 +1,19 @@
+jQuery(document).ready(function() {
+	jQuery('input[title]').each(function() {
+		if(jQuery(this).val() === '') {
+			jQuery(this).val($(this).attr('title'));
+		}
 
+		jQuery(this).focus(function() {
+			if(jQuery(this).val() === jQuery(this).attr('title')) {
+				jQuery(this).val('').addClass('focused');
+			}
+		});
+
+		jQuery(this).blur(function() {
+			if(jQuery(this).val() === '') {
+				jQuery(this).val(jQuery(this).attr('title')).removeClass('focused');
+			}
+		});
+	});
+});
