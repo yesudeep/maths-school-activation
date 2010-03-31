9ec2ff342651432ee20ccac41ff27fe1f2025d8d
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2010-04-01 00:00:44 +0530

    trying out simplemodal

--- media/script/activate.js
+++ media/script/activate.js
@@ -1,18 +1,34 @@
 jQuery(function(){
   var elements = {
     products: jQuery('#products'),
-    product: jQuery('#products a')
+    product: jQuery('#products a'),
+    input_serial_number: jQuery('#input-serial-number')
   };
   
-  var activate_products = [];
-  
+  var activate_products = {},
+      currentProductId = 0;
+
   elements.product.click(function(e){
-    
-    
-    
-    jQuery(this).toggleClass('selected');
+    var elem = jQuery(this),
+        content = elements.input_serial_number,
+        currentProductId = elem.attr('id');
+    content.modal({});
     e.stopPropagation();
     e.preventDefault();
+    return false;
+  });
+  jQuery('#simplemodal-container form.input-serial-number').live('submit', function(e){    
+    var form = jQuery(this),
+      serialNumber = form.find('input[name="serial-number"]').val(),
+      machineId = form.find('input[name="machine-id"]').val();
+
+    activate_products[currentProductId] = {
+      serialNumber: serialNumber,
+      machineId: machineId
+    };
+    console.log(activate_products);
+    jQuery('#' + currentProductId).toggleClass('selected');
+    jQuery.modal.close();
     return false;
   });
 });
