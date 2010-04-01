b0b5d6acc29b456ac3e18bcc469743b303c41f7c
Aswad <aswad.r@gmail.com>
2010-04-01 21:33:01 +0530

    Add overlay form on unsubscription page
    Signed-off-by: Aswad <aswad.r@gmail.com>

---
+++ media/script/unsubscribe.js
@@ -0,0 +1,10 @@
+jQuery(".btn-slide").click(function(){
+	jQuery(".panel").slideToggle("slow");
+});
+jQuery("#cancel-button").click(function(){
+  jQuery(".panel").fadeOut("slow");
+});
+jQuery("#unsubscribe-product-button").click(function(){
+  jQuery(".panel").fadeOut("slow");
+});
+
