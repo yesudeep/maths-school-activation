cde763d830437e8aca48a554b04dafe3fb25c31c
Aswad <aswad.r@gmail.com>
2010-04-02 16:36:20 +0530

    Add profile page and modules for the same
    Signed-off-by: Aswad <aswad.r@gmail.com>

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from google.appengine.ext import db
-from google.appengine.api import memcache
+from google.appengine.ext.db import polymodel
+from google.appengine.api import memcache,users
 from dbhelper import serialize_entities, deserialize_entities, MAX_COUNT, CACHE_DURATION, SerializableModel
 from properties import DecimalProperty
 from decimal import Decimal
@@ -12,7 +13,41 @@ CURRENCY_CHOICES = (
     'USD',
 )
 
+PHONE_NUMBER_TYPE_CHOICES = (
+    'Mobile',
+    'Phone',
+)
+
+class Profile(polymodel.PolyModel):
+    user = db.UserProperty(auto_current_user_add=True)    
+    
+class Person(Profile):
+    first_name = db.StringProperty(required=True)
+    last_name = db.StringProperty(required=True)
+    email = db.EmailProperty(required=True)
+    password = db.StringProperty(required=True)
+    
+class PhoneNumber(db.Model):
+    """Records phonenumber/mobile"""  
+    profile = db.ReferenceProperty(Profile, collection_name='phone_numbers')
+    phone_type = db.StringProperty(choices = PHONE_NUMBER_TYPE_CHOICES)
+    phone_number = db.StringProperty()
+
+class Location(db.Model):
+    """Records the address of a customer."""
+    profile = db.ReferenceProperty(Profile, collection_name='person_locations')    
+    state_or_town = db.StringProperty()
+    suburb = db.StringProperty()
+    street_name = db.StringProperty()
+    postal_code = db.PostalAddressProperty()
+    
+class PersonActivatedProducts(db.Model):
+    """Records all the active products of a customer."""
+    profile = db.ReferenceProperty(Profile, collection_name='active_products')
+    active_products = db.StringListProperty()    
+    
 class Product(SerializableModel):
+    """Displays all the products on Activate Products Page. Use dbhelper.py to import the initial data"""
     title = db.StringProperty()
     subtitle = db.StringProperty()
     up_front_price = DecimalProperty()
@@ -24,4 +59,4 @@ class Product(SerializableModel):
     billing_gst = DecimalProperty()
     billing_gst_currency = db.StringProperty(choices=CURRENCY_CHOICES, default='AUD')
     icon_url = db.URLProperty()
-    
+
