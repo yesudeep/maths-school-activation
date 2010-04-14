90a70af3dfc984c1636cf8d1ba5df992d0940af6
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2010-04-14 14:02:00 +0530

    paypal ipn handling complete.  transactions are now recorded and verified.  completed transactions also tested in sandbox.

--- app/models.py
+++ app/models.py
@@ -22,6 +22,16 @@ PHONE_TYPE_CHOICES = (
     'work',
     'home',
 )
+DEFAULT_PHONE_TYPE = 'mobile'
+
+EMAIL_TYPE_CHOICES = (
+    'personal',
+    'home',
+    'work',
+    'primary',
+    'secondary',
+)
+DEFAULT_EMAIL_TYPE = 'personal'
 
 INVOICE_STATUS_DRAFT = 'draft'
 INVOICE_STATUS_PENDING = 'pending'
@@ -43,16 +53,26 @@ PAYMENT_AGENTS = (
 )
 DEFAULT_PAYMENT_AGENT = PAYMENT_AGENT_PAYPAL
 
+VERIFICATION_STATUS_INVALID = 'invalid'
+VERIFICATION_STATUS_VERIFIED = 'verified'
+VERIFICATION_STATUS_UNKNOWN = 'unknown'
+VERIFICATION_STATUS_CHOICES = (
+    VERIFICATION_STATUS_VERIFIED,
+    VERIFICATION_STATUS_INVALID,
+    VERIFICATION_STATUS_UNKNOWN,
+)
+DEFAULT_VERIFICATION_STATUS = VERIFICATION_STATUS_UNKNOWN
+
 
 class Profile(polymodel.PolyModel):
     """
     This is a base polymodel for models that have contact information.
     Contact information is tied to this polymodel instead of
-    specialized contact information (phone number, email, etc.) 
-    models (eg., CustomerPhoneNumber, PersonAddress, CompanyLocation) 
+    specialized contact information (phone number, email, etc.)
+    models (eg., CustomerPhoneNumber, PersonAddress, CompanyLocation)
     for each of these models that have contact information.
     """
-    
+
     when_created = db.DateTimeProperty(auto_now_add=True)
     when_modified = db.DateTimeProperty(auto_now=True)
 
@@ -60,19 +80,19 @@ class Profile(polymodel.PolyModel):
 class Customer(Profile):
     """
     Customer information.
-    
+
     Helps answer these questions:
-    
+
     1. What is the name of the customer?
     2. When did the customer register?
     3. What orders has the customer placed?
     4. Which products does the customer use?
     5. Which invoices have been issued to a customer?
-    
+
     """
     first_name = db.StringProperty(required=True)
     last_name = db.StringProperty(required=True)
-    
+
     # basic-authentication credentials
     email = db.EmailProperty(required=True)
     password_hash = Base64Property(required=True)
@@ -86,15 +106,21 @@ class Customer(Profile):
 
 
 class Phone(SerializableModel):
-    """Records phone/mobile number of a customer"""  
+    """Records phone/mobile number of a customer"""
     profile = db.ReferenceProperty(Profile, collection_name='phones')
-    phone_type = db.StringProperty(choices = PHONE_TYPE_CHOICES)
+    phone_type = db.StringProperty(choices=PHONE_TYPE_CHOICES, default=DEFAULT_PHONE_TYPE)
     number = db.StringProperty()
+
+
+class Email(SerializableModel):
+    profile = db.ReferenceProperty(Profile, collection_name='emails')
+    email_type = db.StringProperty(choices=EMAIL_TYPE_CHOICES, default=DEFAULT_EMAIL_TYPE)
+    email = db.EmailProperty()
 
 
 class Location(SerializableModel):
     """Records the address of a customer."""
-    profile = db.ReferenceProperty(Profile, collection_name='locations')    
+    profile = db.ReferenceProperty(Profile, collection_name='locations')
     state_or_province = db.StringProperty()
     area_or_suburb = db.StringProperty()
     street_name = db.StringProperty()
@@ -115,14 +141,14 @@ class Product(SerializableModel):
     6. How much is the *monthly* billing price for the product?
     7. How much sales tax will be applied on the billing price?
     8. What image/icon represents this product?
-    
+
     Developer's Note:
     -----------------
     Kindly issue these commands at the admin console to import preliminary data
-    
+
         > from initial_data import import_all
         > import_all()
-    
+
     """
     title = db.StringProperty()
     subtitle = db.StringProperty()
@@ -138,16 +164,16 @@ class Product(SerializableModel):
 class Invoice(SerializableModel):
     """
     Maintains invoice information for orders placed by customers.
-    
+
     Helps answer these questions:
-    
+
     1. What was the total price of the invoice?
     2. Who was the customer?
     3. What currency was used for invoicing?
     4. What was ordered by the customer?
     5. When was the invoice generated?
     6. How many and which transactions were used to satisfy payment for this invoice?
-    
+
     """
     customer = db.ReferenceProperty(Customer, collection_name='invoices')
     total_price = DecimalProperty()
@@ -157,11 +183,11 @@ class Invoice(SerializableModel):
 class Transaction(SerializableModel):
     """
     Records transaction information obtained during the life of
-    a transaction including completion codes and response information 
+    a transaction including completion codes and response information
     from a payment gateway.
-    
+
     Helps answer these questions:
-    
+
     1. Was the transaction successful?
     2. What was the currency used for the transaction?
     3. Who did the transaction?  (Linked via invoice)
@@ -169,17 +195,18 @@ class Transaction(SerializableModel):
     5. When did this transaction occur?
     6. What was the total amount of the transaction as received in the response
        from the payment agent?
-    
+
     """
     identifier = db.StringProperty()
     transaction_type = db.StringProperty()
-    
+
     currency = db.StringProperty(choices=CURRENCY_CHOICES, default=DEFAULT_CURRENCY)
     amount = DecimalProperty()
-    #when_completed = db.DateTimeProperty()    
-    payment_agent = db.StringProperty(choices=PAYMENT_AGENTS, default=DEFAULT_PAYMENT_AGENT)    
-    
+    #when_completed = db.DateTimeProperty()
+    payment_agent = db.StringProperty(choices=PAYMENT_AGENTS, default=DEFAULT_PAYMENT_AGENT)
+
     invoice = db.ReferenceProperty(Invoice, collection_name='transactions')
+    verification_status = db.StringProperty(choices=VERIFICATION_STATUS_CHOICES, default=DEFAULT_VERIFICATION_STATUS)
     data = db.TextProperty()
 
 
